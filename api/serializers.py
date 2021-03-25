a743df3a2438a856ed91f2a527d5c753bfd9f199
Nadezda <nadezdastarodubtseva@yandex.ru>
2021-03-25 14:05:53 +0300

    add Review and Comment serializers

---
+++ api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from api.models import Review, Comment
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        read_only = ['title']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only = ['review']
