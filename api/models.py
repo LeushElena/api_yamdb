4c532fbfc3a5a2ebd42cf7f1ed69910c565246f7
Nadezda <nadezdastarodubtseva@yandex.ru>
2021-03-25 19:51:12 +0300

    add Title model

--- api/models.py
+++ api/models.py
@@ -1,4 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
+
+class Category(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(unique=True, max_length=200)
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.PositiveSmallIntegerField(blank=True, null=True)
+    category = models.ForeignKey(Category, 
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 null=True)
+    description = models.CharField(max_length=1000, blank=True)
+    genre = models.ManyToManyField(Genre,
+                                   related_name="genre_titles")
 
 
 class Review(models.Model):
